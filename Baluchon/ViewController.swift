f4c4fd5ac8979569fabceaf2b2298e8b66448900
Cindy Perat <cindy.perat@gmail.com>
2019-06-17 14:13:06 +0200

    build: init project

---
+++ Baluchon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Baluchon
+//
+//  Created by Cindy Perat on 17/06/2019.
+//  Copyright © 2019 Cindy Perat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
